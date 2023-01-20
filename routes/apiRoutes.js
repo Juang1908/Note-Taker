19c4c6c78a20584dff326b31807a5f561108b9bb
Mr. Galvan <112872430+Juang1908@users.noreply.github.com>
2023-01-19 22:21:52 -0600

    workign on deleting-route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,7 +12,7 @@ return readFile('db/db.json', 'utf-8').then(rawNotes => [].concat(JSON.parse(raw
 //  getNotes().then(notes => console.log(notes))
 // GET
 router.get('/notes', (req, res) => {
-console.log('hit route')
+// console.log('hit route')
    getNotes()
     .then((notes) => {
      res.json(notes)
@@ -34,12 +34,17 @@ getNotes().then(oldNotes =>{
 //  EXtra credit DELETE
 
 //  GETTING AN ERROR
-// router.delete('/notes/:id', (req, res) => {
-//   let notes = JSON.parse(readFile(path.join(__dirname,'..', 'db/db.json')));
-//   notes = notes.filter(note => note.id == req.params.id);
-//     writeFile(path.join(__dirname, '..', 'db/bd.json'), JSON.stringify(notes));
-//     res.json({ message: "Note had been deleted"});
-//   });
+router.delete('/notes/:id', (req, res) => {
+  console.log('deleting notes')
+  getNotes()
+  .then((oldNotes) => {
+  let newNotes = oldNotes.filter(note => note.id !== req.params.id);
+  writeFile('db/db.json', JSON.stringify(newNotes))
+  .then(() => res.json({ msg: 'Success' }))
+  .catch((err) => res.status(500).json(err));
+  })
+  .catch((err) => res.status (500).json(err));
+  });
 
 
   module.exports = router;
